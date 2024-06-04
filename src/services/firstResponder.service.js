02a598f071472b019c8322e0931e48bb1a4a0928
Josinee <ja.besseling@student.avans.nl>
2024-06-04 15:08:18 +0200

    query veranderd

--- src/services/firstResponder.service.js
+++ src/services/firstResponder.service.js
@@ -18,7 +18,8 @@ const firstResponderService = {
         }
 
         const request = new sql.Request(pool);
-        request.query("SELECT * FROM Project WHERE IsActive = 'true'", (error, result) => {
+        request.query("SELECT P.Title, P.Date, P.StartTime, P.EndTime, P.Description, P.Address, P.HouseNr, P.City, P.IsAccepted, P.IsActive,  P.ProjectId, P.PeopleNeeded, COUNT(S.UserId) AS PeopleAssigned FROM Project P LEFT JOIN Shift S ON P.ProjectId = S.ProjectId AND S.IsAssigned = 'true' WHERE P.IsActive = 'true' AND P.PeopleNeeded > 0 GROUP BY P.Title, P.Date, P.StartTime, P.EndTime, P.Description, P.Address, P.HouseNr, P.City, P.IsAccepted, P.IsActive, P.ProjectId, P.PeopleNeeded;",
+         (error, result) => {
             if (error) {
                 logger.error(error);
                 callback(error, null);
