f11493fc450f7a542553a6c28776a04c1797e924
JoepieSnels <j.snels@student.avans.nl>
2024-06-07 13:16:54 +0200

    added all the changable values

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -237,7 +237,6 @@ const loginService = {
 		prepStatement.input("number", sql.NVarChar);
 		prepStatement.input("postCode", sql.NVarChar);
 		prepStatement.input("city", sql.NVarChar);
-		prepStatement.input("role", sql.NVarChar);
 		prepStatement.input("gender", sql.NVarChar);
 		prepStatement.input("dateOfBirth", sql.Date);
 		prepStatement.input("userID", sql.BigInt);
@@ -245,51 +244,42 @@ const loginService = {
 		prepStatement.input("invoiceHouseNr", sql.NVarChar);
 		prepStatement.input("invoiceCity", sql.NVarChar);
 		prepStatement.input("invoiceEmail", sql.NVarChar);
+		prepStatement.input("landLine", sql.NVarChar);
 
 		//alles aanwezig?
-		if (params.firstName && params.lastName && params.emailaddress && params.password && params.newPassword && params.phoneNumber && params.street && params.number && params.postCode && params.city && params.role && params.dateOfBirth && params.dateOfBirth && params.gender) {
-			//sql statement in prep statement zetten met variabelen
-			prepStatement.prepare("UPDATE Member SET FirstName = @firstName, LastName = @lastName, Emailaddress = @emailaddress, Password = @newPassword ,PhoneNumber = @phoneNumber, Street = @street, HouseNr = @number, PostCode = @postCode, City = @city, Role = @role, DateOfBirth = @dateOfBirth, Gender = @gender, InvoiceStreet = @invoiceStreet, InvoiceHouseNr = @invoideHouseNr, InvoiceCity = @invoiceCity, InvoiceEmail = @invoiceEmail WHERE UserId = @userID && Password = @password", (err) => {
+
+		//sql statement in prep statement zetten met variabelen
+		prepStatement.prepare("UPDATE Member SET FirstName = @firstName, LastName = @lastName, Emailaddress = @emailaddress, Password = @newPassword ,PhoneNumber = @phoneNumber, Street = @street, HouseNr = @number, PostCode = @postCode, City = @city,  DateOfBirth = @dateOfBirth, Gender = @gender, InvoiceStreet = @invoiceStreet, InvoiceHouseNr = @invoideHouseNr, InvoiceCity = @invoiceCity, InvoiceEmail = @invoiceEmail, LandLine = @landLine WHERE UserId = @userID && Password = @password", (err) => {
+			if (err) {
+				callback(err, null);
+				logger.error(err);
+			}
+			logger.debug("prepare");
+			// variabelen toevoegen van params naar prepstatement input variabelen en executen
+			prepStatement.execute({ firstName: params.firstName, lastName: params.lastName, emailaddress: params.emailaddress, password: params.password, newPassword: params.newPassword, phoneNumber: params.phoneNumber, street: params.street, number: params.number, postCode: params.postCode, city: params.city, dateOfBirth: params.dateOfBirth, gender: params.gender, userID: userId, landLine: params.landLine, invoiceCity: params.invoiceCity, invoiceStreet: params.invoiceStreet, invoiceHouseNr: params.invoiceHouseNr, invoiceEmail: params.invoiceEmail }, (err, result) => {
+				//TO-DO hardcoded userId eruit(kan nadat user aangemaakt kan worden)
 				if (err) {
 					callback(err, null);
 					logger.error(err);
 				}
-				logger.debug("prepare");
-				// variabelen toevoegen van params naar prepstatement input variabelen en executen
-				prepStatement.execute({ firstName: params.firstName, lastName: params.lastName, emailaddress: params.emailaddress, password: params.password, newPassword: params.newPassword, phoneNumber: params.phoneNumber, street: params.street, number: params.number, postCode: params.postCode, city: params.city, role: params.role, dateOfBirth: params.dateOfBirth, gender: params.gender, userID: userId }, (err, result) => {
-					//TO-DO hardcoded userId eruit(kan nadat user aangemaakt kan worden)
+				logger.debug("execute");
+				//prep statement unprepare zodat connection terug naar pool gaat
+				prepStatement.unprepare((err) => {
+					logger.debug("unprepare");
 					if (err) {
-						callback(err, null);
 						logger.error(err);
+						callback(err, null);
+					} else {
+						logger.info("User updated");
+						callback(null, {
+							status: 200,
+							message: "User updated",
+							data: {},
+						});
 					}
-					logger.debug("execute");
-					//prep statement unprepare zodat connection terug naar pool gaat
-					prepStatement.unprepare((err) => {
-						logger.debug("unprepare");
-						if (err) {
-							logger.error(err);
-							callback(err, null);
-						} else {
-							logger.info("User updated");
-							callback(null, {
-								status: 200,
-								message: "User updated",
-								data: {},
-							});
-						}
-					});
 				});
 			});
-		} else {
-			callback(
-				{
-					status: 400,
-					message: "Required field missing",
-					data: {},
-				},
-				null
-			);
-		}
+		});
 	},
 };
 
