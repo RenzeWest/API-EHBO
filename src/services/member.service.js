851fe83b1d2824522d76bf5c9e88c89a11de18c9
JoepieSnels <j.snels@student.avans.nl>
2024-06-07 11:55:14 +0200

    validatepassword made

--- src/services/member.service.js
+++ src/services/member.service.js
@@ -1,77 +1,81 @@
-const pool = require('../doa/sql-database');
-const logger = require('../util/logger');
-const sql = require('mssql');
+const pool = require("../doa/sql-database");
+const logger = require("../util/logger");
+const sql = require("mssql");
 
 const memberService = {
-    getMember: async (userId, callback) => {
-        logger.trace('MemberService -> getMember');
-            
-        if (!pool.connected) {
-            await pool.connect();
-        }
+	getMember: async (userId, callback) => {
+		logger.trace("MemberService -> getMember");
 
-        // Get a connection fore the prepared statement
-        const prepStatement = new sql.PreparedStatement(pool);
+		if (!pool.connected) {
+			await pool.connect();
+		}
 
-        // Prepare valiables
-        prepStatement.input('userId', sql.BigInt);
+		// Get a connection fore the prepared statement
+		const prepStatement = new sql.PreparedStatement(pool);
 
-        // Bereid het statement door
-        prepStatement.prepare('SELECT * FROM Member WHERE UserId = @userId', err => {
-            if (err) {
-                logger.error(err);
-                callback(err, null);
-                return;
-            }
+		// Prepare valiables
+		prepStatement.input("userId", sql.BigInt);
 
-            prepStatement.execute({userId: userId}, (err, result) => {
-                if (err) {
-                    logger.error(err);
-                    callback(err, null);
-                    return;
-                }
-                logger.debug('getMember -> execute');
+		// Bereid het statement door
+		prepStatement.prepare("SELECT * FROM Member WHERE UserId = @userId", (err) => {
+			if (err) {
+				logger.error(err);
+				callback(err, null);
+				return;
+			}
 
-                // Controlleer of er een gebruiker is gevonden
-                if (result.recordset.length === 0) {
-                    logger.info('No user found');
-                    callback({
-                        status: 404,
-                        message: 'User not found',
-                        data: {}}, null);
+			prepStatement.execute({ userId: userId }, (err, result) => {
+				if (err) {
+					logger.error(err);
+					callback(err, null);
+					return;
+				}
+				logger.debug("getMember -> execute");
 
-                    prepStatement.unprepare(err => {
-                        logger.debug('getMember -> statement unprepared');
-                        if (err) {
-                            logger.error(err);
-                            callback(err, null);
-                        }
-                    });
-                    return;
-                }
+				// Controlleer of er een gebruiker is gevonden
+				if (result.recordset.length === 0) {
+					logger.info("No user found");
+					callback(
+						{
+							status: 404,
+							message: "User not found",
+							data: {},
+						},
+						null
+					);
 
-                // Unprepare statment om connectie vrij te geven
-                prepStatement.unprepare(err => {
-                    logger.debug('getMember -> statement unprepared');
-                    if (err) {
-                        logger.error(err);
-                        callback(err, null);
-                        return;
-                    } else {
-                        logger.info('getMember Succesfull');
-                        const noPassword = result.recordset[0];
-                        delete noPassword.Password;
+					prepStatement.unprepare((err) => {
+						logger.debug("getMember -> statement unprepared");
+						if (err) {
+							logger.error(err);
+							callback(err, null);
+						}
+					});
+					return;
+				}
 
-                        callback(null, {
-                            status: 200,
-                            message: `User found with id ${userId}`,
-                            data: result.recordset[0]
-                        });
-                    }
-                });
-            });
-        });
-    }
-}
+				// Unprepare statment om connectie vrij te geven
+				prepStatement.unprepare((err) => {
+					logger.debug("getMember -> statement unprepared");
+					if (err) {
+						logger.error(err);
+						callback(err, null);
+						return;
+					} else {
+						logger.info("getMember Succesfull");
+						const noPassword = result.recordset[0];
+						delete noPassword.Password;
 
-module.exports =  memberService;
+						callback(null, {
+							status: 200,
+							message: `User found with id ${userId}`,
+							data: result.recordset[0],
+						});
+					}
+				});
+			});
+		});
+	},
+};
+
+module.exports = memberService;
